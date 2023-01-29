e415d5664235ecc54ca4e635d82a72c374f5ca53
anfi3281 <anfi3281@gmail.com>
2023-01-30 04:26:18 +0700

    default

--- resources/views/coba.blade.php
+++
@@ -1,1 +0,0 @@
-<h1 style="text-align:center">Halo, selamat datang di coba.blade.php</h1>
--- routes/web.php
+++ routes/web.php
@@ -17,38 +17,3 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/halo', function () {
-    return 'Halo teman-teman';
-});
-
-Route::get('blog', function () {
-    return view('blog');
-});
-
-Route::get('coba', function () {
-    return view('coba');
-});
-
-Route::get('testing', function () {
-    return "Ini Testing";
-});
-
-Route::get('halo', function () {
-    return view('halo');
-});
-
-Route::get('hy', function () {
-    return "hy juga";
-});
-
-Route::get('/siswa/{nama}/{kelas}', 'App\Http\Controllers\SiswaController@index');
-Route::get('pegawai', 'App\Http\Controllers\PegawaiController@index');
-Route::get('dosen', 'App\Http\Controllers\DosenController@index');
-Route::get('biodata', 'App\Http\Controllers\BiodataController@index');
-Route::get('/name/{nama}', 'App\Http\Controllers\NameController@index');
-Route::get('/biodata2/{kelas}', 'App\Http\Controllers\Biodata2Controller@index');
-Route::get('/input', 'App\Http\Controllers\InputController@formulir');
-Route::post('input/proses', 'App\Http\Controllers\InputController@proses');
-
-Route::get('inputdata', 'App\Http\Controllers\InputDataController@form');
-Route::post('inputdata/proses', 'App\Http\Controllers\InputDataController@proses');
