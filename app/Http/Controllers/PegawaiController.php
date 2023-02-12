b920fb68ffc9ee34a16fc68258284b54cbebb716
anfi3281 <anfi3281@gmail.com>
2023-02-12 13:01:32 +0700

    jadi

---
+++ app/Http/Controllers/PegawaiController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+
+class PegawaiController extends Controller
+{
+    public function tambah(){
+        return view('tambah');
+    }
+
+    public function store(Request $request){
+        DB::table('pegawai')->insert([
+            'pegawai_nama' => $request->nama,
+            'pegawai_jabatan' => $request->jabatan,
+            'pegawai_umur' => $request->umur,
+            'pegawai_alamat' => $request->alamat
+        ]);
+
+        return redirect('/pegawai/tambah');
+    }
+}
---
+++ resources/views/tambah.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Data Pegawai</title>
+</head>
+
+<body>
+    <h1>Employee Data</h1><br>
+    <form action="/pegawai/store" method="post">
+        {{ csrf_field() }}
+        Nama : <input type="text" name="nama" required><br><br>
+        Jabatan : <input type="text" name="jabatan" required><br><br>
+        Umur : <input type="text" name="umur" required><br><br>
+        Alamat : <input type="text" name="alamat" required><br><br>
+        <input type="submit" name="submit" value="Submit">
+    </form>
+</body>
+
+</html>
