c124fb71b62b2f73bb2f128050117f869aba65bf
anfi3281 <anfi3281@gmail.com>
2023-02-27 06:33:08 +0700

    front

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,13 +10,13 @@
 </head>
 
 <body class="bg-primary">
-    <div class="container">
-        <div class="row mt-5 justify-content-center mb-5">
-            <div class="col-lg-12 mb-2">
+    <div class="container-fluid">
+        <div class="row mt-2">
+            <div class="col-lg-4 mb-2">
                 <div class="card">
                     <div class="card-body">
                         <h3 class="text-center">Kelola Footnote</h3>
-                        <form action="/kelola" method="POST">
+                        <form action="/kelola" method="POST" >
                             {{ csrf_field() }}
                             <div class="mt-4 form-group">
                                 <label for="jumlah">Jumlah Penulis : </label>
@@ -57,53 +57,53 @@
                                 </script>
                             </div>
                             <div class="mt-1 form-group">
-                                <label for="nama">Nama : </label>
                                 @if (isset($jumlahpenulis))
                                     @if ($jumlahpenulis == 4)
-                                        <input type="text" class=" form-control-sm" placeholder="Penulis 1"
+                                        <input type="text" class=" form-control" placeholder="Penulis 1"
                                             name="penulis_1">
                                     @else
                                         @for ($i = 1; $i <= $jumlahpenulis; $i++)
-                                            <input type="text" class="form-control-sm"
+                                            <input type="text" class="form-control"
                                                 placeholder="Penulis {{ $i }}"
                                                 name="penulis_{{ $i }}">
                                         @endfor
                                     @endif
                                 @else
-                                    <input type="text" class="form-control-sm" placeholder="Penulis 1"
+                                    <input type="text" class="form-control" placeholder="Penulis 1"
                                         name="penulis_1">
                                 @endif
 
 
                             </div>
-                            <div class="form-group mb-3 mt-2">
-                                <label for="judul">Judul : </label> <br>
-                                <textarea name="judul" class="form-control" id="" cols="30" rows="2" placeholder="Judul"></textarea>
+                            <div class="form-group  mt-3">
+                                <label for="judul">Judul</label> <br>
+                                <textarea name="judul" class="form-control" id="" cols="30" rows="2" placeholder="Tuliskan Judul"></textarea>
                             </div>
-                            <div class="mt-1 form-group">
+                            <div class="mt-3 form-group">
                                 <label for="sumber">Sumber : </label>
                                 <input type="text" class="form-control-sm" name="sumber" placeholder="Sumber">
                             </div>
-                            <div class="mt-1 form-group">
+                            <div class="mt-3 form-group">
                                 <label for="volume">Volume : </label>
                                 <input type="text" class="form-control-sm" name="volume" placeholder="Volume">
                             </div>
-                            <div class="mt-1 form-group">
+                            <div class="mt-3 form-group">
                                 <label for="nomor">Nomor : </label>
                                 <input type="text" class="form-control-sm" name="nomor" placeholder="Nomor">
                             </div>
-                            <div class="mt-1 form-group">
+                            <div class="mt-3 form-group">
                                 <label for="tahun">Tahun : </label>
                                 <input type="text" class="form-control-sm" name="tahun" placeholder="Tahun">
                             </div>
-                            <div class="mt-1 form-group">
+                            <div class="mt-3 form-group">
                                 <label for="halaman">Halaman : </label>
-                                <input type="text" class="w-auto form-control-sm float-left" name="halaman_awal"
+                                <input type="text" class="form-control-sm float-left" style="width:60px;" name="halaman_awal"
                                     placeholder="Awal">
-                                <input type="text" class="w-2 form-control-sm float-left" name="halaman_akhir"
+                                    -
+                                <input type="text" class="form-control-sm float-left" style="width:60px;" name="halaman_akhir"
                                     placeholder="Akhir">
                             </div>
-                            <div class="text-center mt-5 mb-3">
+                            <div class="text-center mt-3">
                                 <input type="submit" name="input" value="Input" class="w-25 btn btn-primary">
                                 <input type="submit" name="edit" value="Edit" class="w-25 btn btn-danger"
                                     style="margin-left:20px">
@@ -112,8 +112,8 @@
                     </div>
                 </div>
             </div>
-            <div class="col-lg-12 mt-3 mb-3">
-                <div class="card">
+            <div class="col-lg-8 mb-3" >
+                <div class="card" style="height: 639px;">
                     <div class="card-body">
                         <h3 class="text-center mb-4">Daftar Footnote</h3>
                         <table class="table table-striped">
